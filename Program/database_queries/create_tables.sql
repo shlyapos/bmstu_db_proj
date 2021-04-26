b2a4cf0da17a379e50c76b10d7b694d789d47810
Alexandr <alexandrsuchkov67@gmail.com>
2021-04-26 14:59:01 +0300

    add queries for create tables

---
+++ Program/database_queries/create_tables.sql
@@ -0,0 +1,82 @@
+DROP TABLE IF EXISTS account CASCADE;
+DROP TABLE IF EXISTS post_text CASCADE;
+DROP TABLE IF EXISTS tag CASCADE;
+DROP TABLE IF EXISTS picture CASCADE;
+DROP TABLE IF EXISTS review CASCADE;
+DROP TABLE IF EXISTS post CASCADE;
+DROP TABLE IF EXISTS post_text CASCADE;
+DROP TABLE IF EXISTS post_pict CASCADE;
+
+DROP TABLE IF EXISTS tag_to_picture CASCADE;
+DROP TABLE IF EXISTS picture_to_post CASCADE;
+
+DROP TABLE IF EXISTS tag_to_text CASCADE;
+DROP TABLE IF EXISTS text_to_post CASCADE;
+
+CREATE TABLE IF NOT EXISTS account (
+	id 	    SERIAL 		NOT NULL PRIMARY KEY,
+    name    VARCHAR(32) NOT NULL,
+	login   VARCHAR(32)	NOT NULL UNIQUE,
+	email 	VARCHAR(64)	NOT NULL UNIQUE,
+    avatar  VARCHAR(32) NOT NULL UNIQUE,
+	salt 	CHAR(32)	NOT NULL UNIQUE,
+	hash	CHAR(64)	NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS post_text (
+    id 	 SERIAL NOT NULL PRIMARY KEY,
+	data TEXT
+);
+
+CREATE TABLE IF NOT EXISTS tag (
+    id 	 SERIAL 	 NOT NULL PRIMARY KEY,
+	name VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS post_pict (
+	id 	 SERIAL 	 NOT NULL PRIMARY KEY,
+	path VARCHAR(64) UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS post (
+	id 	        SERIAL 	NOT NULL PRIMARY KEY,
+	author_id 	INTEGER NOT NULL REFERENCES account(id),
+	rating		INTEGER NOT NULL,
+	public_date	DATE	NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS review (
+	id 	        SERIAL 	NOT NULL PRIMARY KEY,
+	post_id 	INTEGER NOT NULL REFERENCES post(id),
+	auth_id 	INTEGER NOT NULL REFERENCES account(id),
+	review_data	TEXT	NOT NULL,
+	public_date DATE	NOT NULL
+);
+
+
+-- Связующие таблицы для картинок
+CREATE TABLE IF NOT EXISTS tag_to_picture (
+	tag_id INTEGER NOT NULL REFERENCES tag(id),
+	pic_id INTEGER NOT NULL REFERENCES post_pict(id),
+	PRIMARY KEY (tag_id, pic_id)
+);
+
+CREATE TABLE IF NOT EXISTS picture_to_post (
+	post_id INTEGER NOT NULL REFERENCES post(id),
+	pict_id	INTEGER NOT NULL REFERENCES post_pict(id),
+	PRIMARY KEY (post_id, pict_id)
+);
+
+
+-- Связующие таблицы для текста
+CREATE TABLE IF NOT EXISTS tag_to_text (
+	tag_id	INTEGER NOT NULL REFERENCES tag(id),
+	text_id	INTEGER NOT NULL REFERENCES post_text(id),
+	PRIMARY KEY (tag_id, text_id)
+);
+
+CREATE TABLE IF NOT EXISTS text_to_post (
+	post_id INTEGER NOT NULL REFERENCES post(id),
+	text_id	INTEGER NOT NULL REFERENCES post_text(id),
+	PRIMARY KEY (post_id, text_id)
+);
