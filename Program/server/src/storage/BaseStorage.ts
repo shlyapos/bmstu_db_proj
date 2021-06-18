16b94f90355d15b2aeeec6af8a68474307923e44
Alexandr <alexandrsuchkov67@gmail.com>
2021-06-18 21:44:52 +0300

    update storages logic

--- Program/server/src/storage/BaseStorage.ts
+++ Program/server/src/storage/BaseStorage.ts
@@ -5,35 +5,31 @@ export interface IQueryParameters {
 };
 
 export abstract class BaseStorage {
-    public takeAll(tableName: string): Promise<any> {
+    public takeAll(tableName: string, role: string): Promise<any> {
         throw new Error("Method not implemented.");
     }
 
-    public takeById(tableName: string, id: number): Promise<any> {
+    public takeById(tableName: string, id: number, role: string): Promise<any> {
         throw new Error("Method not implemented.");
     }
 
-    public create(tableName: string, params: IQueryParameters): Promise<any> {
+    public create(tableName: string, params: IQueryParameters, role: string): Promise<any> {
         throw new Error("Method not implemented.");
     }
 
-    public update(tableName: string, id: number, newData: any): Promise<any> {
+    public update(tableName: string, params: IQueryParameters, role: string): Promise<any> {
         throw new Error("Method not implemented.");
     }
 
-    public delete(tableName: string, id: number): Promise<any> {
+    public delete(tableName: string, params: IQueryParameters, role: string): Promise<any> {
         throw new Error("Method not implemented.");
     }
 
-    public close(): void {
+    public filterByColumn(tableName: string, params: IQueryParameters, role: string): Promise<any> {
         throw new Error("Method not implemented.");
     }
 
-    public filterByColumn(tableName: string, params: IQueryParameters): Promise<any> {
-        throw new Error("Method not implemented.");
-    }
-
-    public joinTables(tableNameFirst: string, tableNameSecond: string, params: IQueryParameters): Promise<any> {
+    public joinTables(tableNameFirst: string, tableNameSecond: string, params: IQueryParameters, role: string): Promise<any> {
         throw new Error("Method not implemented.");
     }
 };
--- Program/server/src/storage/PgStorage.ts
+++ Program/server/src/storage/PgStorage.ts
@@ -9,60 +9,112 @@ export default class PgStorage extends BaseStorage {
         this.db = PgConnection.getInstance();
     }
 
-    public async takeAll(tableName: string): Promise<any> {
-        return new Promise<any>((resolve, reject) => {
-            this.db.getConnect().query(`SELECT * FROM ${tableName}`,
-                (error, result) => {
+    public async takeAll(tableName: string, role: string): Promise<any> {
+        return await new Promise<any>(async (resolve, reject) => {
+            const client = await this.db.getConnect(role);
+
+            await client.query(`SELECT * FROM ${tableName}`,
+                (error: any, result: { rows: any; }) => {
                     if (error) reject(error);
                     if (result) resolve(result.rows);
-                });
+                }
+            );
+
+            client.release();
         });
     }
 
-    public async takeById(tableName: string, currentId: number): Promise<any> {
-        return new Promise<any>((resolve, reject) => {
-            this.db.getConnect().query(`SELECT * FROM ${tableName} WHERE id = ${currentId}`,
-                (error, result) => {
+    public async takeById(tableName: string, currentId: number, role: string): Promise<any> {
+        return await new Promise<any>(async (resolve, reject) => {
+            const client = await this.db.getConnect(role);
+
+            await client.query(`SELECT * FROM ${tableName} WHERE id = ${currentId}`,
+                (error: any, result: { rows: any; }) => {
                     if (error) reject(error);
                     if (result) resolve(result.rows);
-                })
+                }
+            );
+
+            client.release();
         });
     }
 
-    public async create(tableName: string, params: IQueryParameters): Promise<any> {
-        return new Promise<any>((resolve, reject) => {
-            this.db.getConnect().query(`INSERT INTO ${tableName} ${params.column} VALUES ${params.format}`,
+    public async create(tableName: string, params: IQueryParameters, role: string): Promise<any> {
+        return await new Promise<any>(async (resolve, reject) => {
+            const client = await this.db.getConnect(role);
+
+            await client.query(`INSERT INTO ${tableName} ${params.column} VALUES ${params.format}`,
                 params.values,
-                (error, result) => {
+                (error: any, result: any) => {
+                    if (error) return reject(error);
+                    if (result) return resolve(result);
+                }
+            );
+
+            client.release();
+        });
+    }
+
+    public async delete(tableName: string, params: IQueryParameters, role: string): Promise<any> {
+        return await new Promise<any>(async (resolve, reject) => {
+            const client = await this.db.getConnect(role);
+
+            await client.query(`DELETE FROM ${tableName} WHERE ${params.format}`,
+                params.values,
+                (error: any, result: any) => {
+                    if (error) reject(error);
+                    if (result) resolve(result.rows);
+                }
+            );
+
+            client.release();
+        });
+    }
+
+    public async update(tableName: string, params: IQueryParameters, role: string): Promise<void> {
+        return new Promise<void>(async (resolve, reject) => {
+            const client = await this.db.getConnect(role);
+
+            await client.query(`UPDATE ${tableName} SET ${params.column} ${params.format}`,
+                params.values,
+                (error: any, result: any) => {
                     if (error) reject(error);
                     if (result) resolve(result);
                 });
+
+            client.release()
         });
     }
 
-    public async filterByColumn(tableName: string, params: IQueryParameters): Promise<any> {
-        return new Promise<any>((resolve, reject) => {
-            this.db.getConnect().query(`SELECT * FROM ${tableName} WHERE ${params.format}`,
+    public async filterByColumn(tableName: string, params: IQueryParameters, role: string): Promise<any> {
+        return await new Promise<any>(async (resolve, reject) => {
+            const client = await this.db.getConnect(role);
+
+            await client.query(`SELECT * FROM ${tableName} WHERE ${params.format}`,
                 params.values,
-                (error, result) => {
+                (error: any, result: { rows: any; }) => {
                     if (error) reject(error);
                     if (result) resolve(result.rows);
-                });
+                }
+            );
+
+            client.release();
         });
     }
 
-    public async joinTables(tableNameFirst: string, tableNameSecond: string, params: IQueryParameters): Promise<any> {
-        return new Promise<any>((resolve, reject) => {
-            this.db.getConnect().query(`SELECT * FROM ${tableNameFirst} JOIN ${tableNameSecond} ON ${params.column} ${params.format}`,
+    public async joinTables(tableNameFirst: string, tableNameSecond: string, params: IQueryParameters, role: string): Promise<any> {
+        return await new Promise<any>(async (resolve, reject) => {
+            const client = await this.db.getConnect(role);
+
+            await client.query(`SELECT * FROM ${tableNameFirst} JOIN ${tableNameSecond} ON ${params.column} ${params.format}`,
                 params.values,
-                (error, result) => {
+                (error: any, result: { rows: any; }) => {
                     if (error) reject(error);
                     if (result) resolve(result.rows);
-                });
-        });
-    }
+                }
+            );
 
-    public async close(): Promise<void> {
-        await this.db.getConnect().end();
+            client.release();
+        });
     }
 };
